1197453739900a775dd785500f0b0d2fd6df8220
Siddharth Kumar <144512053+Sid541@users.noreply.github.com>
2025-11-28 18:44:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function updateDateTime() {
+    const now = new Date();
+    document.getElementById("dateTime").textContent = now.toLocaleString();
+}
+
+// call immediately
+updateDateTime();
+
+// update every 1 second
+setInterval(updateDateTime, 1000);
