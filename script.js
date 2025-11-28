153d43e04d0a4c76557824c2a05d2abe4fb64ec4
Siddharth Kumar <144512053+Sid541@users.noreply.github.com>
2025-11-28 18:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 function updateDateTime() {
     const now = new Date();
-    document.getElementById("dateTime").textContent = now.toLocaleString();
+    document.getElementById("timer").textContent = now.toLocaleString();
 }
 
 // call immediately
